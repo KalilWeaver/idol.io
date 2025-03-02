154f579a1fdc7950c11f7ba7abf3d5008886738c
aaronvtn <89952743+aaronvtn@users.noreply.github.com>
2025-03-01 17:44:34 -0800

    Implemented leaderboard and cleaned up UI a bit

--- hello.js
+++ hello.js
@@ -1,32 +1,16 @@
-// JS uses weak typing
-var influence_points = 0
+// team_list contains all information about a team, display_team_list is only a list of names
+const team_list = new Array()
+const display_team_list = new Array()
 
-// This is how to "send" a JS variable to the html web page
-window.onload = function() {
-    document.getElementById("influence_points_display").innerHTML = influence_points
-    
-    document.getElementById("class_testing_display").innerHTML = 0
+// Values to update when page is first loaded
+window.onload = function() { 
+    document.getElementById("points_display").innerHTML = 0
     document.getElementById("team_name_display").innerHTML = "None"
+    document.getElementById("team_list").innerHTML = "Empty"
 }
 
-// // Displays a pop-up that freezes the screen until confirmed
-// window.alert("Your team has " + influence_points + " points!")
-
-// Create functions that can be called back in the html file
-function increase_points() {
-    influence_points++
-
-    // Just for testing, doesn't actually display anything on the web page
-    console.log(influence_points)
-
-    // You have to update the new value of the variable manually
-    document.getElementById("influence_points_display").innerHTML = influence_points
-    // Emoji effects after player clicks button
-    prayer_effects()
-}
 
 // Function to create a prayer emoji effect after pressing prayer button
-
 function prayer_effects() {
     // Creates a span element for the emoji
     let emoji = document.createElement("span")
@@ -37,8 +21,8 @@ function prayer_effects() {
     emoji.style.transition = "transform 1s ease-out, opacity 1s ease-out";
 
    // Hardcoded position for where the prayer button currently is
-    let x = 8; // Exact X position of prayer button
-    let y = 270; // Slighly above the prayer button
+    let x = 55; // Exact X position of prayer button
+    let y = 250; // Slighly above the prayer button
 
     // Sets the position of the emoji
     emoji.style.left = `${x}px`;
@@ -60,7 +44,7 @@ function prayer_effects() {
 
 }
 
-// Experimenting with classes in JS
+// Class for creating a new religion, upgrade_level can refer to which milestone the team has achieved and which buttons they have access to
 class Team {
     constructor(name, point_total, image_file, upgrade_level) {
         this.name = name;
@@ -68,43 +52,41 @@ class Team {
         this.image_file = image_file;
         this.upgrade_level = upgrade_level;
     }
-
     display_name() {
         return this.name;
     }
-
     display_points() {
         return this.point_total;
     }
-
     display_image() {
         return this.image_file;
     }
-
     display_level() {
         return this.upgrade_level;
     }
-
     increase_points(point_display) {
         this.point_total++;
         document.getElementById(point_display).innerHTML = this.point_total
+        prayer_effects()
     }
 }
 
-// // Make a new Team called Testing
-// const Testing = new Team("Testing", 0, "img.png", 1);
 
-
-// Handles form submission for creating a new team
+// Handles form submission for creating a new team and updates the leaderboard
 document.addEventListener('DOMContentLoaded', init, false);
 function init(){
     var team_form = document.getElementById("team_form")
     team_form.addEventListener("submit", function(event) {
         event.preventDefault()
+        
         var team_name = document.getElementById("team_name").value
-        Current = new Team(team_name, 0, "img.png", 1)
-        document.getElementById("team_name_display").innerHTML = Current.display_name()
-        Current.display_points() // Not sure if this does anything
+        display_team_list.push(team_name)
+        team_list.push(new Team(team_name, 0, "img.png", 1))
+
+        team_list.forEach(function(entry) {
+            document.getElementById("team_name_display").innerHTML = entry.display_name()
+        })
+        document.getElementById("team_list").innerHTML = display_team_list.join("<br>")
     })
 };
 
